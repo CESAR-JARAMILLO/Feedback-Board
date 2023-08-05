15718df875cec8abbc19c2599b579c8d90da79ad
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2023-08-05 09:43:44 -0700

    added childs state and onClick

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,20 @@
 import Header from '@/components/mobile/Header'
-import React from 'react'
+import React, { useState } from 'react'
 
 const Home = () => {
+  const [hamburgerClicked, setHamburgerClicked] = useState(false)
+
+  const handleHamburgerClick = (event: React.MouseEvent<HTMLImageElement>) => {
+    event.preventDefault()
+    setHamburgerClicked(!hamburgerClicked);
+  }
   return (
     <>
-    <Header />
-    Home
+      <Header 
+        onClick={handleHamburgerClick} 
+        hamburgerClicked={hamburgerClicked}
+      />
+      Home
     </>
   )
 }
