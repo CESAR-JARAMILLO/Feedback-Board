57cdfea45b56b0894a1e44c0fd33beb451bdd8ea
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2023-08-05 13:20:15 -0700

    renamed component to FilterButton

--- components/mobile/Dropdown.tsx
+++ components/mobile/Dropdown.tsx
@@ -1,6 +1,6 @@
 import { Flex } from '@chakra-ui/react'
 import React from 'react'
-import Inputs from '../suggestions/InputsCard'
+import Inputs from '../suggestions/FilterCard'
 
 const Dropdown = () => {
   return (
