e3c08b508fed8f816db4bec897103ecf06b0e80a
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2023-08-05 01:46:06 -0700

    created and styled Header component - mobile

---
+++ components/mobile/Header.tsx
@@ -0,0 +1,23 @@
+import { Container, Flex, Image, Text } from '@chakra-ui/react'
+import { HamburgerIcon, CloseIcon } from '@chakra-ui/icons'
+
+const Header = () => {
+  return (
+    <Flex 
+      backgroundImage="url(/images/mobile/background-header.png)" 
+      bgRepeat="no-repeat" 
+      bgSize="cover" 
+      py="16px" 
+      px="24px" 
+      justifyContent="space-between"
+    >
+      <Container>
+        <Text fontSize="15px" color="white" fontWeight="bold">Front End Mentor</Text>
+        <Text fontSize="13px" fontWeight="medium" color="white">Feedback Board</Text>
+      </Container>
+      <Image h="17px" mr="2px" my="auto" src='/images/mobile/icon-hamburger.svg'/>
+    </Flex>
+  )
+}
+
+export default Header
