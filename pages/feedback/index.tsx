38a998c9d5b32fe1fe27c0672506e6607e3d6c37
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2023-08-06 14:33:32 -0700

    created feedback pages directory

---
+++ pages/feedback/index.tsx
@@ -0,0 +1,10 @@
+import FeedbackHeader from '@/components/feedback/FeedbackHeader'
+import React from 'react'
+
+const Feedback = () => {
+  return (
+    <FeedbackHeader />
+  )
+}
+
+export default Feedback
