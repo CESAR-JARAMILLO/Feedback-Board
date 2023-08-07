2efa2ec82c2d356c8e57c622b8eba8367bbb4e59
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2023-08-07 15:11:56 -0700

    setup supabase

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,20 @@
 import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
+import { createPagesBrowserClient } from '@supabase/auth-helpers-nextjs'
+import { SessionContextProvider, Session } from '@supabase/auth-helpers-react'
+import { useState } from 'react'
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps }: AppProps<{ initialSession: Session }>) {
+  const [supabaseClient] = useState(() => createPagesBrowserClient())
+
   return (
     <ChakraProvider>
-      <Component {...pageProps} />
+      <SessionContextProvider
+        supabaseClient={supabaseClient}
+        initialSession={pageProps.initialSession}
+      >
+        <Component {...pageProps} />
+      </SessionContextProvider>
     </ChakraProvider>
   )
 }
