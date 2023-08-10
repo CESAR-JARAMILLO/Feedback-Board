f1e0f3a68f0d0ed430be6ea93b35c0d5ad30c50b
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2023-08-09 22:07:02 -0700

    added comments rendering

--- components/comments/Comment.tsx
+++ components/comments/Comment.tsx
@@ -1,16 +1,10 @@
 import { Avatar, Box, Flex, Text } from '@chakra-ui/react'
-import React, { useState } from 'react'
 
-const Comment = () => {
-  const [name, setName] = useState('')
-  const [tag, setTag] = useState('')
-  const [comment, setComment] = useState('')
-  const [avatarUrl, setAvatarUrl] = useState('')
+interface CommentsProps {
+  comment: any;
+}
 
-  const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>) => setName(e.target.value);
-  const handleTagChange = (e: React.ChangeEvent<HTMLInputElement>) => setTag(e.target.value);
-  const handleCommentChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => setComment(e.target.value);
-  const handleAvatarUrlChange = (e: React.ChangeEvent<HTMLInputElement>) => setAvatarUrl(e.target.value);
+const Comment = ({ comment }: CommentsProps) => {
 
   return (
     <Box w="100%" fontSize="13px" p="24px">
@@ -18,14 +12,14 @@ const Comment = () => {
         <Flex gap="16px">
           <Avatar name='Elijah Moss' src='/images/user-images/image-elijah.jpg' />
           <Box my="auto">
-            <Text fontWeight="bold">Elijah Moss</Text>
-            <Text color="#647196" >@hexagon.bestagon</Text>
+            <Text fontWeight="bold">{comment.name}</Text>
+            <Text color="#647196" >{comment.tag}</Text>
           </Box>
         </Flex>
         <Text fontWeight="semibold" color="#4661E6" as="button">Reply</Text>
       </Flex>
       <Text mt="24px" color="#647196">
-        Also, please allow styles to be applied based on system preferences. I would love to be able to browse Frontend Mentor in the evening after my devices dark mode turns on without the bright background it currently has.
+        {comment.comment}
       </Text>
     </Box>
   )
--- components/comments/Comments.tsx
+++ components/comments/Comments.tsx
@@ -1,13 +1,44 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Comment from './Comment'
 import { Flex, Text } from '@chakra-ui/react'
+import { useSupabaseClient } from '@supabase/auth-helpers-react'
 
 const Comments = () => {
+  const [comments, setComments] = useState<any[] | null>(null)
+  const supabase = useSupabaseClient()
+
+  useEffect(() => {
+    const getComments = async () => {
+      try {
+        const { data, error } = await supabase
+          .from('comments')
+          .select()
+  
+        if (error) {
+          console.error("An error occurred:", error);
+          return;
+        }
+        
+        setComments(data)
+  
+      } catch (error) {
+        console.error("Unexpected error:", error);
+      } finally {
+        // Reset loading state
+      }
+    };
+  
+    getComments();
+  }, []);
+
   return (
     <Flex direction="column" bg="#FFF" mt="24px" borderRadius={10}>
       <Text ml="24px" mt="24px" color="#3A4374" fontSize="18px" fontWeight="bold" >4 Comments</Text>
-      <Comment />
-      <Comment />
+      <>
+        {comments?.map((comment, index) => (
+          <Comment key={index} comment={comment} />
+        ))}
+      </>
     </Flex>
   )
 }
