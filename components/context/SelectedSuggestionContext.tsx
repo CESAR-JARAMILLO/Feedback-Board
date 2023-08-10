73b1a8b43f62f30545174f6bfa05242cdbea12de
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2023-08-09 18:12:37 -0700

    updated file

--- components/context/SelectedSuggestionContext.tsx
+++ components/context/SelectedSuggestionContext.tsx
@@ -1,8 +1,27 @@
 import React from 'react'
 
-const SelectedSuggestionContext = React.createContext({
+interface SelectedSuggestionContextValue {
+  selectedSuggestionId: string | null;
+  setSelectedSuggestionId: (id: string) => void;
+}
+
+const SelectedSuggestionContext = React.createContext<SelectedSuggestionContextValue>({
   selectedSuggestionId: null,
   setSelectedSuggestionId: (id: string) => {}
-})
+});
 
-export default SelectedSuggestionContext
+interface SelectedSuggestionProviderProps {
+  children: React.ReactNode;
+}
+
+export const SelectedSuggestionProvider: React.FC<SelectedSuggestionProviderProps> = ({ children }) => {
+  const [selectedSuggestionId, setSelectedSuggestionId] = React.useState<string | null>(null);
+
+  return (
+    <SelectedSuggestionContext.Provider value={{ selectedSuggestionId, setSelectedSuggestionId }}>
+      {children}
+    </SelectedSuggestionContext.Provider>
+  );
+};
+
+export default SelectedSuggestionContext;
