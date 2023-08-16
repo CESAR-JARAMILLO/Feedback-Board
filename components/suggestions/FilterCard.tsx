70460c2074b90e6e5c2276814042c0dd8227f699
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2023-08-15 22:49:07 -0700

    edited with for tabled view

--- components/suggestions/FilterCard.tsx
+++ components/suggestions/FilterCard.tsx
@@ -1,4 +1,4 @@
-import { Flex } from '@chakra-ui/react'
+import { Flex, useMediaQuery } from '@chakra-ui/react'
 import Input from './FilterButton'
 import { useState } from 'react'
 
@@ -6,13 +6,14 @@ const inputs = ['All', 'UI', 'UX', 'Enhancement', 'Bug', 'Feature']
 
 const FilterCard = () => {
   const [selectedInput, setSelectedInput] = useState<string | null>(null)
+  const [isLargerThanMD] = useMediaQuery("(min-width: 768px)");
 
   const handleClick = (inputName: string) => () => {
     setSelectedInput(inputName)
   }
 
   return (
-    <Flex borderRadius={16} p="24px" h="30%" wrap="wrap" gap={2} overflow="hidden" alignItems="center" justifyContent="flex-start" bg="#FFF">
+    <Flex borderRadius={16} p="24px" w={isLargerThanMD ? "240px" : "none" } wrap="wrap" gap={2} overflow="hidden" alignItems="center" justifyContent="flex-start" bg="#FFF">
       {inputs.map((input: string, index: number) => (
           <Input selected={input === selectedInput} onClick={handleClick(input)} key={index} name={input} />
       ))}
--- components/suggestions/RoadmapCard.tsx
+++ components/suggestions/RoadmapCard.tsx
@@ -1,4 +1,4 @@
-import { Box, Flex, Link, Text } from '@chakra-ui/react'
+import { Box, Flex, Link, Text, useMediaQuery } from '@chakra-ui/react'
 import { useRouter } from 'next/router';
 
 interface RoadmapItem {
@@ -13,9 +13,11 @@ const roadmapItems: RoadmapItem[] = [
 ];
 
 const RoadmapCard = () => {
+  const [isLargerThanMD] = useMediaQuery("(min-width: 768px)");
   const router = useRouter()
+
   return (
-    <Flex borderRadius={16} p="24px" gap={6} overflow="hidden" alignItems="center" justifyContent="flex-start" bg="#FFF" direction="column" >
+    <Flex borderRadius={16} w={isLargerThanMD ? "240px" : "none" } p="24px" gap={6} overflow="hidden" alignItems="center" justifyContent="flex-start" bg="#FFF" direction="column" >
       <Flex w="100%" justifyContent="space-between" >
         <Text fontSize="18px" fontWeight="bold" >Roadmap</Text>
         <Link onClick={() => router.push("/roadmap")} fontWeight="semibold" fontSize="13px" textDecoration="underline" color="#4661E6">View</Link>
