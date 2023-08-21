57423f309f4a082cd0cbcbe50f2284dab7a62f5e
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2023-08-21 12:58:23 -0700

    implimented SelectedFilterContext

--- components/suggestions/FilterCard.tsx
+++ components/suggestions/FilterCard.tsx
@@ -1,21 +1,22 @@
 import { Flex, useMediaQuery } from '@chakra-ui/react'
 import Input from './FilterButton'
-import { useState } from 'react'
+import { useContext } from 'react'
+import SelectedFilterContext  from '@/components/context/SelectedFilterContext'
 
 const inputs = ['All', 'UI', 'UX', 'Enhancement', 'Bug', 'Feature']
 
 const FilterCard = () => {
-  const [selectedInput, setSelectedInput] = useState<string | null>(null)
   const [isLargerThanMD] = useMediaQuery("(min-width: 768px)");
+  const { selectedFilter, setSelectedFilter } = useContext(SelectedFilterContext)
 
   const handleClick = (inputName: string) => () => {
-    setSelectedInput(inputName)
+    setSelectedFilter(inputName)
   }
 
   return (
     <Flex borderRadius={16} p="24px" w={isLargerThanMD ? "240px" : "none" } wrap="wrap" gap={2} overflow="hidden" alignItems="center" justifyContent="flex-start" bg="#FFF">
       {inputs.map((input: string, index: number) => (
-          <Input selected={input === selectedInput} onClick={handleClick(input)} key={index} name={input} />
+          <Input selected={input === selectedFilter} onClick={handleClick(input)} key={index} name={input} />
       ))}
     </Flex>
   )
--- components/suggestions/Suggestions.tsx
+++ components/suggestions/Suggestions.tsx
@@ -5,9 +5,11 @@ import { useRouter } from 'next/router'
 import { useSupabaseClient } from '@supabase/auth-helpers-react'
 import EmptySuggestions from './EmptySuggestions'
 import SelectedSuggestionContext  from '@/components/context/SelectedSuggestionContext'
+import SelectedFilterContext from '@/components/context/SelectedFilterContext';
 
 const Suggestions = () => {
   const { selectedSuggestionId, setSelectedSuggestionId } = useContext(SelectedSuggestionContext);
+  const { selectedFilter } = useContext(SelectedFilterContext);
   const [suggestions, setSuggestions] = useState<any[] | null>(null)
   const [isLargerThanMD] = useMediaQuery("(min-width: 768px)");
   const supabase = useSupabaseClient()
@@ -42,19 +44,23 @@ const Suggestions = () => {
     router.push('/feedback')
   };
 
+  const filteredSuggestions = suggestions?.filter((suggestion) => {
+    return selectedFilter === 'All' || suggestion.category === selectedFilter;
+  });
+
   return (
-    <Flex minH="90vh" gap="16px" direction="column" mx={isLargerThanMD ? "none" : "24px"} mt="32px" >
-      {suggestions && suggestions.length > 0 ? (
-        suggestions.map((suggestion, index) => (
+    <Flex minH="90vh" gap="16px" direction="column" mx={isLargerThanMD ? 'none' : '24px'} mt="32px">
+      {filteredSuggestions && filteredSuggestions.length > 0 ? (
+        filteredSuggestions.map((suggestion, index) => (
           <Box onClick={handleClick(suggestion.id)} key={index}>
             <Suggestion suggestion={suggestion} />
           </Box>
         ))
-      ): (
+      ) : (
         <EmptySuggestions />
       )}
     </Flex>
-  )
+  );
 }
 
 export default Suggestions
