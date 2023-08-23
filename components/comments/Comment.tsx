8c02b3d866f43df49c48dce8931ed42bda435b9d
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2023-08-23 11:11:37 -0700

    implimented AddReply and replies rendering logic

--- components/comments/Comment.tsx
+++ components/comments/Comment.tsx
@@ -1,10 +1,23 @@
 import { Avatar, Box, Flex, Text } from '@chakra-ui/react'
+import ReplyComment from './AddReply';
+import { useState } from 'react';
 
 interface CommentsProps {
   comment: any;
 }
 
+interface Reply {
+  reply: string;
+  // include other properties of a reply as needed
+}
+
+
 const Comment = ({ comment }: CommentsProps) => {
+  const [replyDisplay, setReplyDisplay] = useState('none');
+
+  const handleReply = () => {
+    setReplyDisplay(replyDisplay === 'none' ? 'block' : 'none');
+  };
 
   return (
     <Box w="100%" fontSize="13px" p="24px">
@@ -16,11 +29,19 @@ const Comment = ({ comment }: CommentsProps) => {
             <Text color="#647196" >{comment.tag}</Text>
           </Box>
         </Flex>
-        <Text fontWeight="semibold" color="#4661E6" as="button">Reply</Text>
+        <Text onClick={handleReply} fontWeight="semibold" color="#4661E6" as="button">Reply</Text>
       </Flex>
       <Text mt="24px" color="#647196">
         {comment.comment}
       </Text>
+      {comment.replies?.map((reply: Reply, index: number) => (
+        <Text key={index} color="#647196">
+          {reply.reply}
+        </Text>
+      ))}
+      <Flex display={replyDisplay}>
+      <ReplyComment commentId={comment.id} />
+    </Flex>
     </Box>
   )
 }
