b4a6425194630eb35ed659f547649294e70cbfe4
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2023-08-24 20:48:54 -0700

    styled and added data

--- components/comments/Reply.tsx
+++ components/comments/Reply.tsx
@@ -1,13 +1,32 @@
+import { Avatar, Box, Flex, Text } from '@chakra-ui/react';
 import React from 'react'
 
 interface ReplyProps {
-    reply: string;
-
+    reply: any;
+    leftMargin?: string;
 }
 
-const Reply = ({ reply } : ReplyProps) => {
+const Reply = ({ reply, leftMargin } : ReplyProps) => {
+    console.log(reply)
   return (
-    <div>{reply}</div>
+    <Box fontSize="13px" mt="32px" pl="40px">
+      <Flex justifyContent="space-between">
+        <Flex gap="16px">
+          <Avatar name='Elijah Moss' src='/images/user-images/image-elijah.jpg' />
+          <Box my="auto">
+            <Text fontWeight="bold">{reply.name}</Text>
+            <Text color="#647196" >{reply.tag_name}</Text>
+          </Box>
+        </Flex>
+        <Text onClick={() => {}} fontWeight="semibold" color="#4661E6" as="button">Reply</Text>
+      </Flex>
+      <Text mt="24px" ml={leftMargin}color="#647196">
+        {reply.reply}
+      </Text>
+      <Flex>
+      {/* <ReplyComment commentId={comment.id} /> */}
+    </Flex>
+    </Box>
   )
 }
 
